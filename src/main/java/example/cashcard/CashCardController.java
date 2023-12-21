cb2b89e2a112aebbe2300fe62d222e038b4b848d
sikutisa <ilgooinkorea@gmail.com>
2023-12-21 15:09:23 +0900

    use the @PathVariable

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -3,6 +3,7 @@ package example.cashcard;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,11 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 class CashCardController {
 
     @GetMapping("/{requestId}")
-    private ResponseEntity<CashCard> findById() {
-        CashCard cashCard = new CashCard(99L, 123.45);
+    private ResponseEntity<CashCard> findById(@PathVariable Long requestId) {
+
+        if(requestId.equals(99L)) {
+            CashCard cashCard = new CashCard(99L, 123.45);
+
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(cashCard);
+        }
 
         return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(cashCard);
+                .status(HttpStatus.NOT_FOUND)
+                .build();
     }
 }
--- src/test/java/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -32,4 +32,12 @@ class CashcardApplicationTests {
 		assertThat(amount).isEqualTo(123.45);
 	}
 
+	@Test
+	void shouldNotReturnACashCardWithAnUnknownId() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+		assertThat(response.getBody()).isBlank();
+	}
+
 }

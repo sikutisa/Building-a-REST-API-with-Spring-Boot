26d309fa0b74f1daaa714598334cc754266f2015
sikutisa <ilgooinkorea@gmail.com>
2023-12-21 14:52:33 +0900

    add the GET endpoint

---
+++ src/main/java/example/cashcard/CashCardController.java
@@ -0,0 +1,19 @@
+package example.cashcard;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cashcards")
+class CashCardController {
+
+    @GetMapping("/{requestId}")
+    private ResponseEntity<String> findById() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("{}");
+    }
+}

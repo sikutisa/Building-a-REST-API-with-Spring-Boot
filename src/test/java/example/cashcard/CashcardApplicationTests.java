18357105e6585abb5cff4a7018d6ab9456c147ee
sikutisa <ilgooinkorea@gmail.com>
2024-01-07 22:14:50 +0900

    Test the HTTP POST Endpoint

--- src/test/java/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -40,4 +40,13 @@ class CashcardApplicationTests {
 		assertThat(response.getBody()).isBlank();
 	}
 
+	@Test
+	void shouldCreateANewCashCard() {
+		CashCard newCashCard = new CashCard(null, 250.00);
+
+		ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
+
+		assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
+
 }

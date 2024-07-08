4a8a81c7b4a46bf08eb966c51d47a4c403be2a43
sikutisa <ilgooinkorea@gmail.com>
2024-07-08 22:16:51 +0900

    Implement @PutMapping in the Controller

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -65,4 +65,12 @@ class CashCardController {
                         pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))));
         return ResponseEntity.ok(page.getContent());
     }
+
+    @PutMapping("/{requestedId}")
+    private ResponseEntity<Void> putCashCard(@PathVariable Long requestedId, @RequestBody CashCard cashCardUpdate, Principal principal) {
+        CashCard cashCard = cashCardRepository.findByIdAndOwner(requestedId, principal.getName());
+        CashCard updatedCashCard = new CashCard(cashCard.id(), cashCardUpdate.amount(), principal.getName());
+        cashCardRepository.save(updatedCashCard);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/test/java/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -155,10 +155,23 @@ class CashcardApplicationTests {
 	void shouldUpdateAnExistingCashCard() {
 		CashCard cashCardUpdate = new CashCard(null, 19.99, null);
 		HttpEntity<CashCard> request = new HttpEntity<>(cashCardUpdate);
+
 		ResponseEntity<Void> response = restTemplate
 				.withBasicAuth("sarah1", "abc123")
 				.exchange("/cashcards/99", HttpMethod.PUT, request, Void.class);
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+
+		ResponseEntity<String> getResponse = restTemplate
+				.withBasicAuth("sarah1", "abc123")
+				.getForEntity("/cashcards/99", String.class);
+		assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
+		Number id = documentContext.read("$.id");
+		Double amount = documentContext.read("$.amount");
+
+		assertThat(id).isEqualTo(99);
+		assertThat(amount).isEqualTo(19.99);
 	}
 
 }

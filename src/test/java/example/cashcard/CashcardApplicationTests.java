6685d5c140db47320845d8753634fc352474e35e
sikutisa <ilgooinkorea@gmail.com>
2024-07-08 22:29:51 +0900

    Test case: The Cash Card doesn't exist

--- src/test/java/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -201,11 +201,18 @@ class CashcardApplicationTests {
 				.withBasicAuth("sarah1", "abc123")
 				.exchange("/cashcards/99", HttpMethod.DELETE, null, Void.class);
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
-
-		// Add the following code:
+		
 		ResponseEntity<String> getResponse = restTemplate
 				.withBasicAuth("sarah1", "abc123")
 				.getForEntity("/cashcards/99", String.class);
 		assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+	}
+
+	@Test
+	void shouldNotDeleteACashCardThatDoesNotExist() {
+		ResponseEntity<Void> deleteResponse = restTemplate
+				.withBasicAuth("sarah1", "abc123")
+				.exchange("/cashcards/99999", HttpMethod.DELETE, null, Void.class);
+		assertThat(deleteResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 	}
 }
